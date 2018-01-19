1d498855bc755d52ca560d21536c60fedd5f484e
RayGuessford <ray@mpspark.com>
2018-01-19 16:28:33 -0600

    added /wolfram

--- src/main/java/EventHandler.java
+++ src/main/java/EventHandler.java
@@ -4,6 +4,15 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+import com.wolfram.alpha.WAEngine;
+import com.wolfram.alpha.WAException;
+import com.wolfram.alpha.WAImage;
+import com.wolfram.alpha.WAPlainText;
+import com.wolfram.alpha.WAPod;
+import com.wolfram.alpha.WAQuery;
+import com.wolfram.alpha.WAQueryResult;
+import com.wolfram.alpha.WASubpod;
+
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.ReadyEvent;
@@ -11,6 +20,7 @@ import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedE
 import sx.blah.discord.handle.obj.IUser;
 
 public class EventHandler {
+	
 	@EventSubscriber
 	public void readyEvent(ReadyEvent event) {
 		IDiscordClient client = event.getClient(); // Gets the client from the event object
@@ -41,11 +51,68 @@ public class EventHandler {
         		commandDice(event, argsList);
         		break;
         	case "say":
-        		commandSay(event,argsList);
+        		commandSay(event, argsList);
+        		break;
+        	case "wolfram":
+        		commandWolfram(event, argsList);
         		break;
         }
     }
 	
+	private void commandWolfram(MessageReceivedEvent event, List<String> argsList){
+		String queryString = "";
+		for (String string : argsList) {
+			queryString += string + " ";
+		}
+		queryString = queryString.trim();
+		String appid = "T6L59W-62ELH955EP";
+		WAEngine engine = new WAEngine();
+		engine.setAppID(appid);
+		engine.addFormat("image");
+		WAQuery query = engine.createQuery(queryString);
+		 try {
+	            // For educational purposes, print out the URL we are about to send:
+	            System.out.println("Query URL:");
+	            System.out.println(engine.toURL(query));
+	            System.out.println("");
+	            
+	            // This sends the URL to the Wolfram|Alpha server, gets the XML result
+	            // and parses it into an object hierarchy held by the WAQueryResult object.
+	            WAQueryResult queryResult = engine.performQuery(query);
+	            
+	            if (queryResult.isError()) {
+	                System.out.println("Query error");
+	                System.out.println("  error code: " + queryResult.getErrorCode());
+	                System.out.println("  error message: " + queryResult.getErrorMessage());
+	            } else if (!queryResult.isSuccess()) {
+	                System.out.println("Query was not understood; no results available.");
+	            } else {
+	                // Got a result.
+	                System.out.println("Successful query. Pods follow:\n");
+	                for (WAPod pod : queryResult.getPods()) {
+	                    if (!pod.isError()) {
+	                        System.out.println(pod.getTitle());
+	                        System.out.println("------------");
+	                        for (WASubpod subpod : pod.getSubpods()) {
+	                            for (Object element : subpod.getContents()) {
+	                                if (element instanceof WAImage) {
+	                                    Library.sendMessage(event.getChannel(), ((WAImage) element).getURL());
+	                                }
+	                            }
+	                        }
+	                        System.out.println("");
+	                    }
+	                }
+	                // We ignored many other types of Wolfram|Alpha output, such as warnings, assumptions, etc.
+	                // These can be obtained by methods of WAQueryResult or objects deeper in the hierarchy.
+	            }
+	        } catch (WAException e) {
+	            e.printStackTrace();
+	        }
+	}
+	
+
+
 	private void commandSay(MessageReceivedEvent event, List<String> argsList){
 		StringBuilder messageBuilder = new StringBuilder();
 		for (String string : argsList) {
